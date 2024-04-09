14fe6150f24b31fb5a87019dc7823f91e95bcf09
ameliczi <166567779+ameliczi@users.noreply.github.com>
2024-04-10 01:39:09 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('yesButton').addEventListener('click', function() {
+    document.getElementById('response').textContent = '❤!';
+});
+
+document.getElementById('noButton').addEventListener('click', function() {
+    document.getElementById('response').textContent = 'i tak chcesz lol :))';
+});
